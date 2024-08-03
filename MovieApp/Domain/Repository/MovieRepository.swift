56c676a24cfc366bad5b4346acd40a6c85d97216
j.arji <javad.ar2000ir@gmail.com>
2024-08-04 02:56:00 +0330

    implement abstraction movie repository

--- MovieApp/Domain/Repository/MovieRepository.swift
+++ MovieApp/Domain/Repository/MovieRepository.swift
@@ -7,12 +7,34 @@
 
 import Foundation
 
-import Foundation
-
-// MARK: - MovieRepository
+/// MARK: - MovieRepository
 /// This is repository model that it have to implement when we want  have search object (movie , people and Tv show)
 
 protocol MovieRepository {
-
+    //  MARK: - search
+    ///
+    /// Returns search results for movies  based on a query.
+    /// - Parameters:
+    ///    - query: A text query to search for.
+    ///    - page: The page of results to return.
+    ///
+    /// - Throws:  error ``DataTransferError``.
+    ///
+    /// - Returns: Movies matching the query.
+    ///
+    func search(query: String, page: Int) async throws -> PaginationDTO<MovieDTO>
+    
+    //  MARK: - detail
+    ///
+    /// Returns search results for detail movie based on a id.
+    /// - Parameters:
+    ///    - id: movie id
+    ///
+    ///
+    /// - Throws:  error ``DataTransferError``.
+    ///
+    /// - Returns: Movie matching the id.
+    ///
+    func detail(id: String) async throws -> MovieDTO
 }
 
